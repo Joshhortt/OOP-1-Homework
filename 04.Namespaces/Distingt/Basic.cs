018eb9f497dd7b07c7decfcaf642601cf07cf7ce
Joshhortt <joshhortt@yahoo.com>
2021-03-04 12:01:18 +0000

    Add Homework 04. about changing Namespaces within an Application.

---
+++ 04.Namespaces/Distingt/Basic.cs
@@ -0,0 +1,12 @@
+﻿namespace Mathematical
+{
+    public class Basic
+    {
+        public static double PI = 3.141592653589;
+
+        public static double GetPi()
+        {
+            return PI;
+        }
+    }
+}
---
+++ 04.Namespaces/Distingt/Program.cs
@@ -0,0 +1,19 @@
+﻿// HOMEWORK - 04 -  Namespaces
+//Create a class file and change the namespaces to be different. Call a method within that class.
+
+using System;
+using Mathematical;
+
+namespace Distingt
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(Basic.PI);
+			Console.WriteLine(Basic.GetPi());
+			Console.WriteLine(Mathematical.Basic.PI);
+			
+		}
+	}
+}
