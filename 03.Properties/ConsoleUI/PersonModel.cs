802baf1a2ed827e3be84a86afde6175d42f4c916
Joshhortt <joshhortt@yahoo.com>
2021-03-04 13:49:55 +0000

    Add Homework 03. about Properties.

---
+++ 03.Properties/ConsoleUI/PersonModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public class PersonModel
+    {
+		public string StreetAddress { get; set;  }
+		public int ZipCode { get; set; }
+		public string City { get; set; }
+		public string State { get; set; }
+
+		public string FullAddress
+		{
+			get  //read-only property
+			{
+				return $"My Full Address is: { StreetAddress} { ZipCode } {  City } { State }";
+			}
+		}
+	}
+}
---
+++ 03.Properties/ConsoleUI/Program.cs
@@ -0,0 +1,27 @@
+﻿// HOMEWORK - 03 -  Properties
+// Create a Console Application with a class that has properties (street address, zipcode, city, state),
+// then add a full property that is read-only.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonModel person = new PersonModel();  
+            person.StreetAddress = "Rua D 14 -";
+            person.ZipCode = 1900;
+            person.City = "- Lisbon";
+            person.State = "- Estremadura";
+
+            Console.WriteLine(person.FullAddress);
+            
+            Console.ReadLine();
+        }
+    }
+}
